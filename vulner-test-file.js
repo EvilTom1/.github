6ca0363d2475161a6ac2d6a3d5a3ba39cf9d3238
dupicate-test <duplicate941@gmail.com>
2025-05-06 20:49:36 +0530

    vulnerable test file for code scanning

--- vulner-test-file.js
+++ vulner-test-file.js
@@ -1,40 +1,8 @@
-// Vulnerable JavaScript code for testing GitHub code scanning
-const http = require('http');
-const url = require('url');
+const { exec } = require('child_process');
 
-// Simulate a user database
-const users = {
-  admin: 'supersecretpassword',
-  user: 'password123'
-};
-
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url, true).query;
-  const username = queryObject.username;
-  const password = queryObject.password;
-
-  // Vulnerable to hardcoded credentials check (CWE-798)
-  if (users[username] && users[username] === password) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Welcome, ' + username);
-  } else {
-    res.writeHead(401, { 'Content-Type': 'text/plain' });
-    res.end('Unauthorized');
-  }
-
-  // Vulnerable to command injection (CWE-77)
-  const { exec } = require('child_process');
-  const unsafeInput = queryObject.cmd;
-  exec(unsafeInput, (error, stdout, stderr) => {
-    if (error) {
-      res.write('Error: ' + stderr);
-    } else {
-      res.write('Output: ' + stdout);
-    }
-    res.end();
+require('http').createServer(function (req, res) {
+  let command = req.url.substring(1); // simulate untrusted input
+  exec(command, function (err, stdout, stderr) {
+    res.end(stdout);
   });
-});
-
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+}).listen(8080);
